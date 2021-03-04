72ca34f444629e4d2ce99f78c2e12af1abb4adff
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2021-03-03 21:36:40 -0800

    filter task

--- app.py
+++ app.py
@@ -149,22 +149,23 @@ def image_filter():
         # TODO: Get the user's chosen filter type (whichever one they chose in the form) and save
         # as a variable
         # HINT: remember that we're working with a POST route here so which requests function would you use?
-        filter_type = ''
-        filter_chosen = requests.post('http://127.0.0.1:5000/image_filter')
+        filter_type = request.form.get('filter_type')
         # Get the image file submitted by the user
         image = request.files.get('users_image')
 
         # TODO: call `save_image()` on the image & the user's chosen filter type, save the returned
         # value as the new file path
-
+        new_file_path = save_image(image, filter_type)
         # TODO: Call `apply_filter()` on the file path & filter type
-
+        apply_filter(new_file_path, filter_type)
         image_url = f'./static/images/{image.filename}'
 
         context = {
             # TODO: Add context variables here for:
             # - The full list of filter types
+            'filter_types': filter_types,
             # - The image URL
+            'image_url': image_url
         }
 
         return render_template('image_filter.html', **context)
